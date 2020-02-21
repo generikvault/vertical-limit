c2bb93b38225c99680261d84f8afc88f27b18f88
Jan Erik Keller <jan-erik.keller@helmsauer-gruppe.de>
2020-02-21 13:19:32 +0100

    added draft of jump, select and new cursor commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,159 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+'use strict';
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vertical-jump" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let up = vscode.commands.registerCommand('extension.vertical-jump.cursorBlockFirstLine', () => {
+		let editor = vscode.window.activeTextEditor;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		if (editor == null) {
+			return;
+		}
+
+		var bp = new BlockPosition(editor.document, editor.selection.start)
+
+		while (bp.dec());
+
+		editor.selections = [bp.selection];
 	});
 
-	context.subscriptions.push(disposable);
+	let down = vscode.commands.registerCommand('extension.vertical-jump.cursorBlockLastLine', () => {
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor == null) {
+			return;
+		}
+
+		var bp = new BlockPosition(editor.document, editor.selection.end)
+
+		while (bp.inc());
+
+		editor.selections = [bp.selection];
+	});
+
+	let selectUp = vscode.commands.registerCommand('extension.vertical-jump.cursorBlockFirstLineSelect', () => {
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor == null) {
+			return;
+		}
+
+		var bp = new BlockPosition(editor.document, editor.selection.start)
+
+		while (bp.dec());
+
+		editor.selections = [bp.selectionFromStart];
+	});
+
+	let selectDown = vscode.commands.registerCommand('extension.vertical-jump.cursorBlockLastLineSelect', () => {
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor == null) {
+			return;
+		}
+
+		var bp = new BlockPosition(editor.document, editor.selection.end)
+
+		while (bp.inc());
+
+		editor.selections = [bp.selectionFromStart];
+	});
+
+	let insertUp = vscode.commands.registerCommand('extension.vertical-jump.multiCursorBlockFirstLine', () => {
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor == null) {
+			return;
+		}
+
+		var bp = new BlockPosition(editor.document, editor.selection.start)
+
+		let selections = []
+		while (bp.dec())
+			selections.push(bp.selection)
+		editor.selections = selections;
+	});
+
+	let insertDown = vscode.commands.registerCommand('extension.vertical-jump.multiCursorsBlockLastLine', () => {
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor == null) {
+			return;
+		}
+
+		var bp = new BlockPosition(editor.document, editor.selection.end)
+
+		let selections = []
+		while (bp.inc())
+			selections.push(bp.selection)
+		editor.selections = selections;
+	});
+
+	context.subscriptions.push(up, down, selectUp, selectDown, insertUp, insertDown);
+}
+
+class BlockPosition {
+	private doc: vscode.TextDocument
+	private pos: vscode.Position
+	private line: number
+	private inside = false
+	private indent: number
+
+	constructor(doc: vscode.TextDocument, pos: vscode.Position) {
+		this.doc = doc
+		this.pos = pos
+		this.line = pos.line
+		this.indent = this.indentAt(this.line)
+	}
+
+	inc(): boolean {
+		let line = this.line + 1
+		if (line >= this.doc.lineCount)
+			return false
+		return this.jump(line)
+	}
+
+	dec(): boolean {
+		let line = this.line - 1
+		if (line < 0)
+			return false
+		return this.jump(line)
+	}
+
+	jump(line: number): boolean {
+		let matchs = this.indentAt(line) == this.indent
+		this.inside = this.inside || matchs
+		if (!matchs && this.inside)
+			return false
+
+		this.line = line
+		return true
+	}
+
+	private indentAt(line: number): number {
+		return this.doc.lineAt(line).firstNonWhitespaceCharacterIndex
+	}
+
+	get position(): vscode.Position {
+		return new vscode.Position(this.line, this.pos.character);
+	}
+
+	get selection(): vscode.Selection {
+		var pos = this.position
+		return new vscode.Selection(pos, pos);
+	}
+
+	get selectionFromStart(): vscode.Selection {
+		return new vscode.Selection(this.pos, this.position);
+	}
+
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
